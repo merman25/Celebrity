8484310d2d9376286d4ce63dde868cf41051ccd9
merman25 <merman25@protonmail.com>
2020-05-02 11:26:04 +0200

    Simplified test definitions: instead of one file per player, have one player file which takes the player index as an environment variable

---
+++ client/cypress/integration/games/rejoin-restored-game-end-of-round/player.js
@@ -0,0 +1,25 @@
+import * as common from "../../common-test-functions";
+import * as defs from "./game-defs";
+
+const index = Cypress.env('PLAYER_INDEX');
+
+describe('Initialisation', () => {
+    it('Checks environment variables are set', () => {
+        assert.typeOf(index, 'number', 'PLAYER_INDEX should be set to a number');
+    });
+});
+
+describe(`Player ${index + 1}`, () => {
+    it('Plays a game', () => {
+        cy.visit('http://192.168.1.17:8080/celebrity.html');
+
+        const { playerName, otherPlayers, celebrityNames, iAmHosting, hostName } = common.retrievePlayerParameters(index, defs.playerNames, defs.celebrityNames);
+
+        if (iAmHosting) {
+            common.hostRestoredGame(index, playerName, iAmHosting, hostName, defs.gameID, otherPlayers, celebrityNames, defs.turnIndexOffset, defs.turns);
+        }
+        else {
+            common.joinRestoredGame(index, playerName, iAmHosting, hostName, defs.gameID, otherPlayers, celebrityNames, defs.turnIndexOffset, defs.turns);
+        }
+    });
+});
