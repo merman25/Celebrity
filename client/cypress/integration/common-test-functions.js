9ca48cf599734ee53a58e8fefa77787744f3270e
merman25 <merman25@protonmail.com>
2020-05-03 15:56:28 +0200

    Simplified testing code: removed redundant files

--- client/cypress/integration/common-test-functions.js
+++ client/cypress/integration/common-test-functions.js
@@ -1,5 +1,30 @@
 import { DOMSpecs } from "./dom-specs.js";
 
+export function openSiteAndPlayGame(gameSpec) {
+    describe('Initialisation', () => {
+        it('Checks environment variables are set', () => {
+            assert.typeOf(gameSpec.index, 'number', 'PLAYER_INDEX should be set to a number');
+        });
+    });
+
+    describe(`Player ${gameSpec.index + 1}`, () => {
+        it('Plays a game', () => {
+            cy.visit('http://192.168.1.17:8080/celebrity.html');
+
+            const clientState = retrievePlayerParameters(gameSpec.index, gameSpec.playerNames, gameSpec.celebrityNames);
+            clientState.index = gameSpec.index;
+            clientState.gameID = gameSpec.gameID;
+            clientState.turnIndexOffset = gameSpec.turnIndexOffset;
+            clientState.turns = gameSpec.turns;
+            if (gameSpec.customActions)
+                clientState.customActions = gameSpec.customActions;
+            clientState.restoredGame = gameSpec.restoredGame;
+
+            playGame(clientState);
+        });
+    });
+}
+
 export function retrievePlayerParameters(index, playerNames, celebrityNames) {
     const hostName = playerNames[0];
     const playerName = playerNames[index];
@@ -38,7 +63,7 @@ export function playGame(clientState) {
 
     checkDOMContent(DOMSpecs, clientState);
     if (!clientState.restoredGame) {
-        assert('[id="teamList"]', and(isVisible, not(hasContent)), { describeExpected: 'visible and empty' });
+        myAssert('[id="teamList"]', and(isVisible, not(hasContent)), { describeExpected: 'visible and empty' });
     }
 
     // Give everyone time to check the DOM content before it changes
@@ -190,7 +215,7 @@ export function and(...predicates) {
         return true;
     };
 }
-export function assert(selector, predicate, { unique = true, describeExpected = 'ok' } = {}) {
+export function myAssert(selector, predicate, { unique = true, describeExpected = 'ok' } = {}) {
     cy.get(selector)
         .then(selectedElements => {
             /* Presumed bug in Cypress, which I've decided is a nice feature.
---
+++ client/cypress/integration/games/04-players.js
@@ -0,0 +1,51 @@
+import * as common from "../common-test-functions";
+
+const index = Cypress.env('PLAYER_INDEX');
+const g = 'got-it';
+const p = 'pass';
+const e = 'end-turn';
+
+
+const gameSpec = {
+    index: index,
+    restoredGame: false,
+    playerNames: ['Marvin the Paranoid Android', 'Bender', 'Johnny 5', 'R2D2'],
+    gameID: '4795',
+    celebrityNames: [
+        ['Neil Armstrong', 'Marilyn Monroe', 'John F. Kennedy', 'Audrey Hepburn', 'Paul McCartney', 'Rosa Parks'],
+        ['Hippolyta', 'Alexander the Great', 'Hypatia', 'Xerxes', 'Helen of Troy', 'Plato'],
+        ['Carl Friedrich Gauss', 'Leonhard Euler', 'John von Neumann', 'Kurt Gödel', 'Gottfried Leibniz', 'Joseph Fourier'],
+        ['Emily Blunt', 'Emma Thompson', 'Judi Dench', 'Carey Mulligan', 'Cate Blanchett', 'Rhea Seehorn']
+    ],
+    turnIndexOffset: 0,
+
+    // 24 total
+    turns: [
+        [g, g, p, g, p, g, g, e], // 5
+        [g, g, g, g, e], // 9
+        [p, g, g, g, g, g, g, e], // 15
+        [g, g, g, p, e], // 18
+        [e], //18
+        [g, g, g, g, e], // 22
+        [g, g], // 24
+
+        [g, g, g, g, e], // 9
+        [g, g, g, g, e], // 22
+        [g, g, p, g, p, g, g, e], // 5
+        [g, g, e], // 24
+        [g, g, g, p, e], // 18
+        [e], //18
+        [p, g, g, g, g, g, g], // 15
+
+        [g, g, p, g, p, g, g, e], // 5
+        [e], //18
+        [g, g, g, g, e], // 9
+        [g, g, e], // 24
+        [p, g, g, g, g, g, g, e], // 15
+        [g, g, g, g, e], // 22
+        [g, g, g], // 18
+    ],
+
+}
+
+common.openSiteAndPlayGame(gameSpec);
