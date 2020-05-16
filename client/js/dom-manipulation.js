dd46fadeeeb5988eba3565012de1d5a496aa3aaf
merman25 <merman25@protonmail.com>
2020-05-16 11:16:10 +0200

    Minor cleanup

--- client/js/dom-manipulation.js
+++ client/js/dom-manipulation.js
@@ -1,78 +1,82 @@
 const selectorsToShowOrHide = [
 	{
 		selector: '#divJoinOrHost',
-		styleFn: (myGameState, serverGameState) => myGameState.myName && !serverGameState.gameID ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => myGameState.myName && !serverGameState.gameID,
 	},
 	{
 		selector: '#divChooseName',
-		styleFn: (myGameState, serverGameState) => !myGameState.myName ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => !myGameState.myName,
 	},
 	{
 		selector: '#joinGameForm',
-		styleFn: (myGameState, serverGameState) => myGameState.willJoin && !serverGameState.gameID ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => myGameState.willJoin && !serverGameState.gameID,
 	},
 	{
 		selector: '#join',
-		styleFn: (myGameState, serverGameState) => myGameState.myName && !serverGameState.gameID && !myGameState.willJoin && !myGameState.willHost ? '' : 'none',
+		styleFn: (myGameState, serverGameState) => myGameState.myName && !serverGameState.gameID && !myGameState.willJoin && !myGameState.willHost,
+		visibleDisplay: '',
 	},
 	{
 		selector: '#host',
-		styleFn: (myGameState, serverGameState) => myGameState.myName && !serverGameState.gameID && !myGameState.willJoin && !myGameState.willHost ? '' : 'none',
+		styleFn: (myGameState, serverGameState) => myGameState.myName && !serverGameState.gameID && !myGameState.willJoin && !myGameState.willHost,
+		visibleDisplay: '',
 	},
 	{
 		selector: '#hostGameDiv',
-		styleFn: (myGameState, serverGameState) => myGameState.myName && serverGameState.gameID && myGameState.iAmHosting ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => myGameState.myName && serverGameState.gameID && myGameState.iAmHosting,
 	},
 	{
 		selector: '#startTurnButton',
-		styleFn: (myGameState, serverGameState) => serverGameState.status === 'READY_TO_START_NEXT_TURN' && myGameState.iAmPlaying ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => serverGameState.status === 'READY_TO_START_NEXT_TURN' && myGameState.iAmPlaying,
 	},
 	{
 		selector: '#startGameButton',
-		styleFn: (myGameState, serverGameState) => myGameState.iAmHosting && serverGameState.status === 'WAITING_FOR_NAMES' && serverGameState.numPlayersToWaitFor === 0 ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => myGameState.iAmHosting && serverGameState.status === 'WAITING_FOR_NAMES' && serverGameState.numPlayersToWaitFor === 0,
 	},
 	{
 		selector: '#gameParamsForm',
-		styleFn: (myGameState, serverGameState) => myGameState.myName && serverGameState.gameID && myGameState.iAmHosting && !myGameState.gameParamsSubmitted && !serverGameState.rounds ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => myGameState.myName && serverGameState.gameID && myGameState.iAmHosting && !myGameState.gameParamsSubmitted && !serverGameState.rounds,
 	},
 	{
 		selector: '#requestNamesButton',
 		styleFn: (myGameState, serverGameState) =>
 			myGameState.myName
-				&& serverGameState.gameID
-				&& myGameState.iAmHosting
-				&& serverGameState.rounds
-				&& serverGameState.teams
-				&& serverGameState.teams.length > 0
-				&& !myGameState.namesRequested
-				&& serverGameState.status === 'WAITING_FOR_PLAYERS' ? 'block' : 'none',
+			&& serverGameState.gameID
+			&& myGameState.iAmHosting
+			&& serverGameState.rounds
+			&& serverGameState.teams
+			&& serverGameState.teams.length > 0
+			&& !myGameState.namesRequested
+			&& serverGameState.status === 'WAITING_FOR_PLAYERS',
 	},
 	{
 		selector: '#allocateTeamsDiv',
 		styleFn: (myGameState, serverGameState) =>
 			myGameState.myName
-				&& serverGameState.gameID
-				&& myGameState.iAmHosting
-				&& !myGameState.namesRequested
-				&& serverGameState.status === 'WAITING_FOR_PLAYERS' ? 'block' : 'none',
+			&& serverGameState.gameID
+			&& myGameState.iAmHosting
+			&& !myGameState.namesRequested
+			&& serverGameState.status === 'WAITING_FOR_PLAYERS',
 	},
 	{
 		selector: '#turnControlsDiv',
-		styleFn: (myGameState, serverGameState) => serverGameState.status === 'PLAYING_A_TURN' && myGameState.iAmPlaying ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => serverGameState.status === 'PLAYING_A_TURN' && myGameState.iAmPlaying,
 	},
 	{
 		selector: '#startNextRoundButton',
-		styleFn: (myGameState, serverGameState) => myGameState.iAmHosting && serverGameState.status === 'READY_TO_START_NEXT_ROUND' ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => myGameState.iAmHosting && serverGameState.status === 'READY_TO_START_NEXT_ROUND',
 	},
 	{
 		selector: '#nameList',
-		styleFn: (myGameState, serverGameState) => serverGameState.status === 'WAITING_FOR_NAMES' && !myGameState.namesSubmitted ? 'block' : 'none',
+		styleFn: (myGameState, serverGameState) => serverGameState.status === 'WAITING_FOR_NAMES' && !myGameState.namesSubmitted,
 	},
 ];
 
 const setDOMElementVisibility = function (myGameState, serverGameState) {
-	selectorsToShowOrHide.forEach(({ selector, styleFn }) => {
-		const display = styleFn(myGameState, serverGameState);
+	selectorsToShowOrHide.forEach(({ selector, styleFn, visibleDisplay }) => {
+		const display = !styleFn(myGameState, serverGameState) ?         'none' :
+                                      visibleDisplay != null   ? visibleDisplay :
+                                                                         'block';
 		document.querySelectorAll(selector)
 			.forEach(element => element.style.display = display);
 	});
