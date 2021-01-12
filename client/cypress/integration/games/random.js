2755f24e2e7009882df79c20e16a8a968622918e
merman25 <merman25@protonmail.com>
2021-01-12 23:01:12 +0100

    Added some more player names, removed commented code

--- client/cypress/integration/games/random.js
+++ client/cypress/integration/games/random.js
@@ -4,7 +4,8 @@ const g = 'got-it';
 const p = 'pass';
 const e = 'end-turn';
 
-const playerNames = ['Marvin the Paranoid Android', 'Bender', 'Johnny 5', 'R2D2', 'HAL 9000', 'Wall-E', 'T-1000', 'Soundwave', 'Robocop', 'CHAPPiE'];
+const playerNames = ['Marvin the Paranoid Android', 'Bender', 'Johnny 5', 'R2D2', 'HAL 9000', 'Wall-E', 'T-1000', 'Soundwave', 'Robocop', 'CHAPPiE',
+                    'Data', 'C3PO', 'Arnie', 'Toadborg', 'Sentinel', 'Samantha', 'Dot Matrix', 'Optimus Prime', 'Deep Thought'];
 // 317 names, taken largely from real games
 const celebNames = ['(Oom) Thijs', 'Ada Lovelace', 'Adolf Hitler', 'Agatha Christie', 'Alan Turing', 'Alan Winde', 'Alastair Darling', 'Alexander the Great', 'Ali B.', 'Ali g', 'Alladin', 'Andy Warhol', 'Angela Merkel', 'Anne Frank', 'Annie mg schmidt', 'Arnie', 'Arnold Schwarzenegger', 'Athol fugard', 'Audrey Hepburn', 'Ayrton Senna', 'Bambi', 'Barbara Windsor', 'Batman', 'Beadle', 'Beethoven', 'Benedict Cumberbatch', 'Beyoncé', 'Big Ears', 'Bilbo Baggins', 'Bill Bryson', 'Billie Eilish', 'Blue', 'Bob Dylan', 'Bojo', 'Boris', 'Boris Johnson', 'Boy George', 'Brad Pitt', 'Bridget Jones', 'Buddha', 'Burgemeester Stulemeijer', 'Buzz Aldrin', 'Captain Blackbeard', 'Captain Hook', 'Captain Jack Sparrow', 'Caravaggio', 'Carey Mulligan', 'Carl Friedrich Gauss', 'Carl Rogers', 'carol vorderman', 'Cate Blanchett', 'Catwoman', 'Charles Darwin', 'Charles glass', 'Cher', 'Chris Hoy', 'Chris Packham', 'Chris Whitty', 'Cinderella', 'Clint Eastwood', 'Colin Firth', 'Dali Lama', 'Darth Vader', 'Dave', 'Dave benson phillips', 'David Attenborough', 'David Beckham', 'David Copperfield', 'Delia Smith', 'Deon Meyer', 'Derren Brown', 'Dido Harding', 'Dieuwertje blok', 'Dieuwertje Blok', 'Diewertje blok', 'Dirk scheeringa', 'Dolly Parton', 'Dominic Cummings', 'Donald duck', 'Donald Duck', 'Donald trump', 'Donald Trump', 'dr zeus', 'Dua lipa', 'Eddie Redmayne', 'Eeyore', 'Elisabeth Warren', 'Elon Musk', 'Elton John', 'Elvis', 'Emily Blunt', 'Emma Thompson', 'Enid Blyton', 'Ernie (van Bert)', 'Erwin Schrodinger', 'ET', 'Ewan McGregor', 'Father Christmas', 'fearne cotton', 'Federer', 'Femke Halsema', 'Florence nightingale', 'Frank sinatra', 'Fred west', 'Freddy Mercury', 'Freek de jonge', 'Freek vonk', 'Galileo', 'Gandalf The Grey', 'Gandhi', 'Gareth Malone', 'Gary glitter', 'Gary Player', 'George Monbiot', 'Getafix', 'Gollum', 'Good King Wenceslas', 'Gordon Ramsey', 'Gorilla', 'Gottfried Leibniz', 'Greta thunberg', 'Hamlet', 'Hansie Cronje', 'Harry Houdini', 'Harry Potter', 'Harry styles', 'Helen of Troy', 'Henny huisman', 'Hillary Swank', 'Hippolyta', 'Hitler', 'Houdini', 'Hugh Jackman', 'Hypatia', 'Iain bolt', 'Ian Hislop', 'Inspector Gadget', 'irvine welsh', 'Isaac Newton', 'Ivanka trump', 'J R R Tolkien', 'Jacinda adern', 'Jacinda Ardern', 'Jack Nicholson', 'Jack the Ripper', 'Jair balsonaro', 'James Bond', 'James Joyce', 'Jamie Oliver', 'Jan', 'Jane Austen', 'Jane Eyre', 'jay z', 'Jemima Puddleduck', 'Jennifer Anisten', 'Jeremy Paxman', 'Jerry Seinfeld', 'Jezus', 'Jildo', 'Jippe', 'JK Rowling', 'Johan Cruijff', 'John F. Kennedy', 'John Le Carre', 'John Maynard Keynes', 'John Oliver', 'John von Neumann', 'Johnny Walker', 'Jonty Rhodes', 'Joseph Fourier', 'Judas', 'Judi Dench', 'Julia Donaldson', 'Justin Timberlake', 'Kiki Bertens', 'Klaas', 'Kristen Stewart', 'Kurt Gödel', 'Kwik, Kwek & Kwak', 'Lassie', 'leona lewis', 'Leonardo de Vinci', 'Leonhard Euler', 'lewis carol', 'Lewis Carroll', 'lisa simpson', 'Lloyd christmas', 'Loch Ness Monster', 'Lord Nelson', 'Luke Skywalker', 'Madame Bovary', 'Maddog Mattis', 'Madonna', 'Malala', 'Malcolm X', 'Margaret Thatcher', 'Marilyn Manson', 'Marilyn Monroe', 'Mark Zuckerberg', 'Martin Luther', 'Martin Luther King', 'Mary', 'Mary of Nazareth', 'Mary Poppins', 'Maya de bij', 'McGuyver', 'Meis', 'Melissa (Tribe)', 'Meryl Streep', 'Michael Jackson', 'Michael Jordan', 'Michael knight', 'Michaelangelo', 'Michel Barnier', 'michelle obama', 'Michelle Obama', 'Mickey mouse', 'Miles Davis', 'Mother Teresa', 'Mother Theresa', 'Mr Bean', 'Napoleon', 'Ned Flanders', 'Neil Armstrong', 'Noah (vriendje v Mischa)', 'Noddy', 'Noel edmonds', 'Norah Jones', 'Odie (uit Garfield)', 'Oom Dagobert', 'Oprah', 'Oprah Winfrey', 'Oscar Wilde', 'Otto', 'Paashaas', 'Papa Smurf', 'Paul McCartney', 'Paul Mescal (Connell in Normal People)', 'Paulien Cornelisse', 'Peppa Pig', 'Peter pan', 'Peter R de Vries', 'Philip Pullman', 'Phoebe Buffet', 'Picasso', 'Piet', 'Piglet', 'Pink Floyd', 'Plato', 'Pocahontas', 'Prince Harry', 'R2D2', 'Rasputin', 'Remy', 'Rhea Seehorn', 'Ricky Gervais', 'Roald Dahl', 'Robin', 'Robin Williams', 'Robyn Williams', 'Roger Daltry', 'Roger federa', 'Rosa Parks', 'Rowan atkinson', 'Rudolph the red nose reindeer', 'russel brand', 'Russell Crowe', 'Ruth Bader Ginsberg', 'Ryan Renolds', 'Sam Neill', 'Samuel L Jackson', 'Sarah Jessica Parker', 'Scrooge', 'Sebastian (lobster in Little Mermaid)', 'Shakespeare', 'Shakira', 'Sharon Dijksma (burgemeester Utrecht))', 'Sherlock Holmes', 'Shrek', 'Sigmund Freud', 'Simba', 'Sinterklaas', 'Siya Kolisi', 'Smaug', 'Sneeuwwitje', 'Snow White', 'Spiderman', 'Stephen Fry', 'Stephen Hawkins', 'Steve Jobs', 'Stevie Wonder', 'Taylor Swift', 'Tess Daley', 'The Gruffalo', 'The Queen (Elizabeth 2)', 'The Very Hungry Caterpillar', 'Thea Beckman', 'Thijs', 'Thijs van leer', 'Thomas the Tank Engine', 'Tigger', 'Tobias Funke', 'Tom Cruise', 'Tony Soprano', 'Trump', 'Ulysses', 'Ursula von der Leyen', 'Venus', 'Vladamir Putin', 'Whitey Houston', 'William Shakespeare', 'Winnie the Pooh', 'Winston Churchill', 'Xerxes', 'Yoda', 'Zoe Ball', 'Zwarte Piet (Black Pete)'];
 
@@ -41,7 +42,6 @@ export const generateGame = function (numPlayers, options = {
             currentTurn = null;
             for (let nameIndex = 0; nameIndex < totalNames;) {
                 if (currentTurn === null) {
-                    // console.log(`computing turn at index ${turns.length}`);
                     currentTurn = [];
                     turns.push(currentTurn);
                 }
@@ -50,11 +50,7 @@ export const generateGame = function (numPlayers, options = {
                     const roundDurationInSec = 60;
                     let roundMarginInSec = 3;
                     if (!options.fastMode && options.fullChecksWhenNotInFastMode) {
-                        // console.log('using larger margin');
                         roundMarginInSec = 10;
-                    }
-                    else {
-                        // console.log(`fastMode ${options.fastMode}, fullChecks ${options.fullChecksWhenNotInFastMode}, using smaller margin`);
                     }
                     const playDurationInSec = roundDurationInSec - roundMarginInSec;
 
@@ -67,7 +63,6 @@ export const generateGame = function (numPlayers, options = {
                         currentTurn.push(playDurationForThisName);
 
                         if (playDurationSoFar < playDurationInSec) {
-                            // console.log(`playDurationForThisName ${playDurationForThisName} takes us to total duration ${playDurationSoFar}, clicking another button`);
                             let randomResult = random();
                             if (randomResult < 0.1) {
                                 currentTurn.push(p);
@@ -82,7 +77,6 @@ export const generateGame = function (numPlayers, options = {
                             }
                         }
                         else {
-                            // console.log(`playDurationForThisName ${playDurationForThisName} takes us to total duration ${playDurationSoFar}, won't click`);
                             currentTurn = null;
                         }
                     }
@@ -111,24 +105,6 @@ export const generateGame = function (numPlayers, options = {
     }
     else if (options.turnVersion === 'V2') {
         turnsV2 = true;
-        // const totalGotItClicks = numRounds * totalNames;
-        // for (let clickIndex=0; clickIndex < totalGotItClicks; ) {
-        //     let waitDuration = 5 + Math.floor(20 * random());
-        //     turnsV2.push(waitDuration);
-
-        //     const randomResult = random();
-        //     if (randomResult < 0.1) {
-        //         turnsV2.push(p);
-        //     }
-        //     else {
-        //         turnsV2.push(g);
-        //         clickIndex++;
-
-        //         if (clickIndex % totalNames == 0) {
-        //             turnsV2.push('round-ended');
-        //         }
-        //     }
-        // }
     }
 
     const gameSpec = {
