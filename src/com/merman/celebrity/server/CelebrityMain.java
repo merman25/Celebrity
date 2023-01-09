02e89e4119e8826c27d1fd72ed9b02e9696b5e13
merman25 <merman25@protonmail.com>
2023-01-09 10:40:35 +0100

    Set Locale explicitly to US to get consistent formatting in logs on different test machines (mainly decimal and thousands separators)

--- src/com/merman/celebrity/server/CelebrityMain.java
+++ src/com/merman/celebrity/server/CelebrityMain.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -40,6 +41,7 @@ public class CelebrityMain {
 	public static void main(String[] args) throws IOException {
 		GameManager.deleteExisting = true;
 		GameManager.createFiles = true;
+		Locale.setDefault(Locale.US);
 		
 		List<File> gameFileList = new ArrayList<>();
 
